909778df7bc17524299e357942dad0ffe4a6cad3
JacopoLombardi <lombajacopo@gmail.com>
2024-05-30 10:15:28 +0200

    [add] migration wine

---
+++ app/Models/Wine.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wine extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'wine',
+        'winery',
+        'rating_average',
+        'rating_reviews',
+        'location',
+        'image'
+    ];
+}
---
+++ database/migrations/2024_05_30_081430_create_wines_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wines', function (Blueprint $table) {
+            $table->id();
+            $table->string('winery', 50);
+            $table->string('wine', 100);
+            $table->decimal('rating_average', 2, 1);
+            $table->string('rating_reviews');
+            $table->string('location');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wines');
+    }
+};
